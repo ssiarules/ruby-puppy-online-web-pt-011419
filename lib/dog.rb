2386a8f0c01d73ef87ab30c3417bd128939db23a
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-23 10:27:37 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,36 @@
+class Dog 
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << name 
+  end 
+  
+  def self.clear_all 
+    self.all.clear 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.class_variable_get(name_of_class_variable)
+    @@all 
+  end 
+  
+  # def self.all 
+  #   self.each do |name| 
+  #     puts self 
+  #   end 
+  # end 
+  # @name
+#end 
+
+Dog.all.each do |dog|
+  puts "#{dog.name}"
+end
+end 
+
+ #dog = Dog.new
+# Dog.new = name
